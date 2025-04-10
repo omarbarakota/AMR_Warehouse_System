51a21296158960ad96b84b9a50db28eb248089e0
omarbarakota <omar.barkat731@gmail.com>
2025-04-10 19:05:04 +0200

    Added: test_login.py

---
+++ app/tests/test_login.py
@@ -0,0 +1,13 @@
+import pytest
+from main import app
+
+def test_login_success():
+    client = app.test_client()
+    response = client.post('/login', data={'username': 'admin', 'password': '123'}, follow_redirects=True)
+    assert response.status_code == 200
+    #assert b"dashboard" in response.data or b"index" in response.data
+
+def test_login_failure():
+    client = app.test_client()
+    response = client.post('/login', data={'username': 'admin', 'password': 'wrongpass'})
+    assert b"Invalid username or password" in response.data
