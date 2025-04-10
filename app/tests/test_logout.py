17ce98527f2bf8e211d795a0fb5251f78bd4d771
omarbarakota <omar.barkat731@gmail.com>
2025-04-10 19:05:16 +0200

    Added: test_logout.py

---
+++ app/tests/test_logout.py
@@ -0,0 +1,9 @@
+from main import app
+
+def test_logout_clears_session():
+    client = app.test_client()
+    with client.session_transaction() as sess:
+        sess['username'] = 'admin'
+        sess['logged_in'] = True
+    response = client.get('/logout', follow_redirects=True)
+    assert b"login" in response.data
