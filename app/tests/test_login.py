9bad35b324fddf4a4c08b32d5171b071e73c2828
omarbarakota <omar.barkat731@gmail.com>
2025-04-10 19:34:28 +0200

    Modified added mock for unit tests

--- app/tests/test_login.py
+++ app/tests/test_login.py
@@ -1,6 +1,13 @@
 import pytest
 from main import app
 
+# Example in test_login.py (using pytest-mock)
+from unittest.mock import patch
+
+def test_login(mocker):
+    mocker.patch("your_module.connection_function", return_value=True)
+    # Your test logic here
+
 def test_login_success():
     client = app.test_client()
     response = client.post('/login', data={'username': 'admin', 'password': '123'}, follow_redirects=True)
--- app/tests/test_receive.py
+++ app/tests/test_receive.py
@@ -1,5 +1,12 @@
 from main import app
 
+# Example in test_login.py (using pytest-mock)
+from unittest.mock import patch
+
+def test_login(mocker):
+    mocker.patch("your_module.connection_function", return_value=True)
+    # Your test logic here
+
 def test_receive_message():
     client = app.test_client()
     with client.session_transaction() as sess:
